f5798fa598fd30a4e214639048647cd86aa0b32c
Kayla S <kaylasmart.wma@gmail.com>
2020-02-29 17:08:41 -0500

    added photos to slideshow

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-var images = [ "images2/blackgold.png","images2/IMG_2146.jpg", "images2/IMG_2144.png",
+var images = [ "images2/shortcropf.png", "images2/shortcropb.png", "images2/redshort.jpg",
+  "images2/blackgold.png",
+ "images2/IMG_2146.jpg",
+"images2/IMG_2144.png",
 "images2/IMG_2143.png",
 "images2/IMG_2260.jpg",
 "images2/IMG_2261.png",
